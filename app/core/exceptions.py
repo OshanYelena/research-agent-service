113ad2b2dd4feb821822a6f19fe8c1113544f239
oshanYelena <oshanranasinghe25@gmail.com>
2026-05-12 22:16:54 +0530

    feat: exception handler added

---
+++ app/core/exceptions.py
@@ -0,0 +1,13 @@
+class AppError(Exception):
+    def __init__(
+        self,
+        message: str,
+        status_code: int = 500,
+        error_code: str = "APP_ERROR",
+        details: dict | None = None,
+    ):
+        self.message = message
+        self.status_code = status_code
+        self.error_code = error_code
+        self.details = details or {}
+        super().__init__(message)
